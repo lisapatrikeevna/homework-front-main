1548244d2804d8ab3c3518e137890a8321001533
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2023-09-14 23:35:56 +0200

    <HW1 />

---
+++ src/HW/2/UserList2.tsx
@@ -0,0 +1,26 @@
+import {CurrentUser} from './CurrentUser';
+import {UsersObjectType, UserType} from './HW2';
+
+type UserList2PropsType = {
+	users: any // пропиши типизацию
+	filterUsers: ()=>void // пропиши типизацию
+};
+
+export const UserList2 = (props: UserList2PropsType) => {
+    const filterUsers=()=>{
+        props.filterUsers()
+    }
+	return (
+		<div id={'hw02-users'}>
+			<h2>User List:</h2>
+
+			<button id={'hw02-filter-button'} onClick={filterUsers}>SHOW ME FRIENDS FROM LA</button>
+
+			<ul>
+			  {props.users.myFriends.map((user:UserType) => (
+			    <CurrentUser user={user}/>
+			  ))}
+			</ul>
+		</div>
+	);
+};
